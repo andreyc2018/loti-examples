cda111c2664ecd8b6f17bbf2a4c47db3034f7f44
Andrey Chichinov <acv4@3ds.com>
2023-12-20 22:39:23 -0500

    Improve error if io_uring is not supported.

--- probe.c
+++ probe.c
@@ -64,7 +64,7 @@ int main() {
     printf("This program won't work on kernel versions earlier than 5.6\n");
     struct io_uring_probe *probe = io_uring_get_probe();
     if (probe == NULL) {
-        perror("Error probing io_uring");
+        printf("Unable to get io_uring probe. The kernel does not support io_uring.\n");
         return 1;
     }
     printf("Report of your kernel's list of supported io_uring operations:\n");
